834163b36a4ba0e7b4588f93bf678a8f9cbfdcaa
Gajan_L <73145375+Gajan-L@users.noreply.github.com>
2021-02-12 09:41:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from graph import create_graph, dijkstra, a_star
+from count_time import timeit
+
+if __name__ == '__main__':
+    # read graph file and create the graph
+    data_dir = "D:/Project/AI/Project 1/input/"
+    graph = create_graph(data_dir)
+
+    # get the start and end points
+    start, end = random.sample(graph.nodes, 2)
+
+    path_1 = dijkstra(start, end, graph)
+    print(f"Shortest path from {start} to {end}:", path_1)
+    print(" ")
+    path_2 = a_star(start, end, graph)
+    print(f"Shortest path from {start} to {end}:", path_2)
+
