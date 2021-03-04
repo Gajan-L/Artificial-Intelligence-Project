edccd75d9ccb17b1abbe2096d34d33af9d294432
Gajan_L <73145375+Gajan-L@users.noreply.github.com>
2021-03-04 10:04:31 +0800

    Update main.py

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,43 +1,56 @@
-import graph as g
-from search import backtrack
+from csp import Constraint, CSP, backtracking_search
+from graph import Graph
+import operator
 
+class GraphColoringConstraint(Constraint[int, int]):
+    def __init__(self, vertex1: int, vertex2: int) -> None:
+        super().__init__([vertex1, vertex2])
+        self.vertex1: int = vertex1
+        self.vertex2: int = vertex2
+    def satisfied(self, assignment):
+        # If either vertex is not in the assignment then it is not
+        # yet possible for their colors to be conflicting
+        if self.vertex1 not in assignment or self.vertex2 not in assignment:
+            return True
+        # check the color assigned to vertex2 is not the same as the color assigned to vertex2
+        return assignment[self.vertex1] != assignment[self.vertex2]
 
 def main():
-    f = open("input/gc_78317094521100.txt", "r")
+    f = open("input/gc_1378296846561000.txt","r")
     Lines = f.readlines()
     f.close()
     print("Reading files")
-    color, node_list = readFile(Lines)
+    graph, color = readFile(Lines)
     print("CSP graph coloring.")
-    csp = {}
-    for k in node_list.keys():
-        csp[k] = -1
-    result = backtrack(csp, node_list, color)
-    print("Result of CSP Graph Coloring: ")
-    if result:
-        print(result)
+    variables = graph.vertices()
+    domains = {}
+    for variable in variables:
+        domains[variable] = [i for i in range(color)]
+    csp = CSP(variables, domains)
+    for constraint in graph.edges():
+        (vertex1, vertex2) = tuple(constraint)
+        csp.add_constraint(GraphColoringConstraint(vertex1, vertex2))
+    assignment = {}
+    solution = backtracking_search(csp, assignment, domains)
+    if solution is None:
+        print("No solution!")
     else:
-        print("No solution was found.")
-    if check(csp, node_list):
+        print(sorted(solution.items(), key=operator.itemgetter(0)))
+    if check(solution, graph):
         print("Algorithm correct!")
     else:
         print("Something went wrong!")
 
-
-
-def check(csp, node_list):
-    for i in range(len(node_list)):
-        if node_list.get(i):
-            node = node_list.get(i)
-            for neighbor in node.neighbors:
-                if csp[neighbor.index] == csp[node.index]:
-                    print(neighbor.index, node.index)
-                    return False
+def check(solution, graph):
+    for edge in graph.edges():
+        (vertex1, vertex2) = tuple(edge)
+        if solution[vertex1] == solution[vertex2]:
+            print(vertex1, vertex2)
+            return False
     return True
 
-
 def readFile(Lines):
-    node_list = {}
+    g = Graph()
     color = -1
     for line in Lines:
         if line.startswith("#"):
@@ -48,16 +61,10 @@ def readFile(Lines):
             x = line.strip("\n").split(",")
             x[0] = int(x[0])
             x[1] = int(x[1])
-            n1 = g.Nodes(x[0], color) if x[0] not in node_list else node_list.get(x[0])
-            n2 = g.Nodes(x[1], color) if x[1] not in node_list else node_list.get(x[1])
-            node_list[x[0]] = n1
-            node_list[x[1]] = n2
-            if n2 not in n1.neighbors:
-                n1.add_neighbor(n2)
-            if n1 not in n2.neighbors:
-                n2.add_neighbor(n1)
-    return color, node_list
-
+            g.add_vertex(x[0])
+            g.add_vertex(x[1])
+            g.add_edge((x[0], x[1]))
+    return g, color
 
 if __name__ == "__main__":
     main()
