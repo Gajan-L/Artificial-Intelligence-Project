a56d64288a1a891329f6c01dcd45df24258bc5cc
Gajan_L <73145375+Gajan-L@users.noreply.github.com>
2021-04-30 01:45:42 +0800

    Update main.py
    update the checking movement function

--- Project_4/main.py
+++ Project_4/main.py
@@ -4,6 +4,7 @@ import pickle
 import time
 from qLearning import QLearningAgent
 
+
 # loading the visit history
 def visit_num():
     try:
@@ -17,12 +18,14 @@ def visit_num():
         print(visit)
         return visit
 
+
 # storing the visit history
 def store_visit(v):
     pickle.dump(v, open('visit', 'wb'))
     print('Updating visit history')
 
-# decide which world for entering
+
+# decide which world to enter
 def decide_world(v):
     world = 1
     visited = v[world]
@@ -31,6 +34,29 @@ def decide_world(v):
             world = i
             visited = v[i]
     return world
+
+
+# check the real movement
+# there is a chance that agent does not go where you decide to go
+def check_real_move(state,newState):
+    # actions = ['N', 'S', 'E', 'W']
+    newX = newState[0]
+    newY = newState[1]
+    x = state[0]
+    y = state[1]
+    if newX == x:
+        check_y = newY - y
+        if check_y == 1:
+            return 0
+        elif check_y == -1:
+            return 1
+    else:
+        check_x = newX - x
+        if check_x == 1:
+            return 2
+        elif check_x == -1:
+            return 3
+
 
 # traverse a world until exit
 def traverse(api, agent):
@@ -47,12 +73,13 @@ def traverse(api, agent):
         if world == -1:# break when exit a world
             break
         action = agent.get_action(state)
-        print('Agent goes to {}'.format(agent.actions[action]))
         newState, reward = api.make_move(agent.actions[action], world)
         time.sleep(15)
         newStateX = int(newState['x'])
         newStateY = int(newState['y'])
         newState = [newStateX, newStateY]
+        action = check_real_move(state, newState)
+        print('Agent goes to {}'.format(agent.actions[action]))
         agent.learn(state, action, reward, newState)
         state = newState
 
@@ -60,10 +87,8 @@ def traverse(api, agent):
 if __name__ == "__main__":
     api = Api(1077, 1287)
     visit = visit_num()
-    # enter each world for 5 times
     for i in range(50):
         current_world = int(api.get_location())
-        print(current_world)
         if current_world in range(1, 11):
             print('Agent in world {}'.format(current_world))
             agent = QLearningAgent(current_world)
